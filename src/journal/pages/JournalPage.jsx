d76c25e54b7c75fc05b846b98b29ab41e1f2605f
Camilo López <camiloj.lopez79@gmail.com>
2023-03-30 15:53:37 -0300

    NothingSelectedView - No hay nada seleccionado

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,14 +1,11 @@
-import {Typography} from '@mui/material';
 import { JournalLayout } from '../layout';
+import {NothingSelectedView} from '../views';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lectus purus, varius at enim at, aliquam ornare ex. In suscipit orci at metus bibendum pharetra. Proin ut purus vitae magna fringilla porttitor.
-      </Typography>
 
-      { /* NothingSelected */ }
+      <NothingSelectedView />
       { /* NoteView */ }
     </JournalLayout>
   );
