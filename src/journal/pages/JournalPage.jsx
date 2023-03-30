08c93a202144a4124564c7804b560c52d4094903
Camilo López <camiloj.lopez79@gmail.com>
2023-03-30 16:30:18 -0300

    Botón Flotante

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,33 @@
+import { IconButton } from '@mui/material';
+import { AddOutlined } from '@mui/icons-material';
+// Layout
 import { JournalLayout } from '../layout';
+// Views
 import {NoteView, NothingSelectedView} from '../views';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
 
-      { /*<NothingSelectedView />*/ }
-      <NoteView />
+      <NothingSelectedView />
+      { /*<NoteView />*/ }
+
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          position: 'fixed',
+          right: 50,
+          bottom: 50,
+          ':hover': {
+            backgroundColor: 'error.main',
+            opacity: 0.8
+          }
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }}/>
+      </IconButton>
     </JournalLayout>
   );
 }
