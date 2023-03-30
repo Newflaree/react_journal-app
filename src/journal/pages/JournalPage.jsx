f3aca51e44920d32eaa851d84a044272f8a86347
Camilo López <camiloj.lopez79@gmail.com>
2023-03-30 13:47:26 -0300

    JournalLayout y JournalPage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,6 +1,15 @@
+import {Typography} from '@mui/material';
+import { JournalLayout } from '../layout';
 
 export const JournalPage = () => {
   return (
-    <h1>JournalPage</h1>
+    <JournalLayout>
+      <Typography>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lectus purus, varius at enim at, aliquam ornare ex. In suscipit orci at metus bibendum pharetra. Proin ut purus vitae magna fringilla porttitor.
+      </Typography>
+
+      { /* NothingSelected */ }
+      { /* NoteView */ }
+    </JournalLayout>
   );
 }
